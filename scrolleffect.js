a5436bb825b86b3b4e4f633886d23b006ffee697
Olle LD <olledavegardh@gmail.com>
2024-05-03 16:15:59 +0200

    Changes

--- scrolleffect.js
+++ scrolleffect.js
@@ -1,10 +1,11 @@
-$(function(){
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 100){
-            $('header').addClass('fixed-header');
-        }
-        else{
-            $('header').removeClass('fixed-header');
-        }
-    });
+// script.js
+document.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const screenHeight = window.innerHeight;
+    let scale = 1;
+
+    if (scrollTop < screenHeight) {
+        scale = 1 - scrollTop / screenHeight;
+        document.querySelector('.svg-container').style.transform = `scale(${scale})`;
+    }
 });
